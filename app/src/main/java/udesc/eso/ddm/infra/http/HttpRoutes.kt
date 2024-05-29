498c20e85c304459bda7196ce5bc05fedfecba32
Gustavo Avelar <108141733+GusDev0258@users.noreply.github.com>
2024-05-29 11:24:16 -0300

    Update HttpRoutes.kt

--- app/src/main/java/udesc/eso/ddm/infra/http/HttpRoutes.kt
+++ app/src/main/java/udesc/eso/ddm/infra/http/HttpRoutes.kt
@@ -1,7 +1,7 @@
 package udesc.eso.ddm.infra.http
 
 object HttpRoutes {
-    private const val URL_HOME = "http://192.168.1.102:3000/"
-    private const val URL_UDESC = "http://10.60.90.141:3000/"
+    private const val URL_HOME = "http://seuipcasa:3000/"
+    private const val URL_UDESC = "http://ipnudesc:3000/"
     const val TRANSLATE_ROUTE = "${URL_HOME}generations/translate-word"
 }
